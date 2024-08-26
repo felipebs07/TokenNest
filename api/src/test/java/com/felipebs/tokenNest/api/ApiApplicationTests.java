28838a77dfad107b2fec089b0a32d197fbf81113
Felipebs <07felipebs07@gmail.com>
2024-08-25 22:47:06 -0300

    Add Directory api (TokenNest API) without build

---
+++ api/src/test/java/com/felipebs/tokenNest/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipebs.tokenNest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
